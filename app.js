5a80f83b04e0420c1e47f61af0c5b53ec5331828
BrainGhost <juniorbalamage@gmail.com>
2021-08-19 17:32:12 +0300

    Fifth commits saved(changes background :: but have not set them to the localstorage)

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+//On load
+const loading = document.querySelector(".loading");
+window.addEventListener("load", () => {
+  loading.className += " hidden"; //the space is necessary unless it wont work
+  // loading.classList.add("hidden");
+  // loading.style.display = "none";
+});
+
 // Declaration of variable
 
 const MenuBurger = document.querySelector(".menu-burger");
@@ -96,11 +104,53 @@ function deleteCheck(e) {
 //Fetch API
 
 //Store to local Storage
+//get request
+// fetch("")
+//   //handle success
+//   .then((response) => response.json())
+//   //convert to json
+//   .then((json) => {
+//     console.log(json);
+//   }) //print data to console
+//   .catch((err) => {
+//     alert.log("Could not fetch the API");
+//   }); //catch errors
 
-//On load
-const loading = document.querySelector(".loading");
-window.addEventListener("load", () => {
-  loading.className += " hidden"; //the space is necessary unless it wont work
-  // loading.classList.add("hidden");
-  // loading.style.display = "none";
+//function to change bg-colors
+const box_time = document.querySelector(".box-time");
+
+const bgColor = document.querySelectorAll(".bg-color span");
+bgColor.forEach((span) => {
+  span.addEventListener("click", (event) => {
+    const active_btn = document.querySelectorAll(".active");
+    active_btn.forEach((e) => {
+      e.className = e.className.replace("active", "");
+    });
+    event.target.classList.add("active");
+    const link_change = document.querySelector("#bg-link-change");
+
+    const link = event.target.classList;
+    //Store colors for the background
+    localStorage.setItem("themes", link[1]);
+
+    if (link.contains("active")) {
+      if (link[1] === "blue-color") {
+        link_change.href = "./styles.css";
+      }
+      if (link[1] === "red-color") {
+        link_change.href = "./themes/red.css";
+      }
+      if (link[1] === "yellow-color") {
+        link_change.href = "./themes/yellow.css";
+      }
+      if (link[1] === "green-color") {
+        link_change.href = "./themes/green.css";
+      }
+    } else {
+      alert("Message: Are sure your code is correct??LOL");
+    }
+  });
 });
+//theme on window refresh
+const colorFromLocalStorage = localStorage.getItem("themes");
+console.log(colorFromLocalStorage);
