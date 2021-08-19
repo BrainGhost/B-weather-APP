1266e052e872640d0ba5a43855b5a56e72270149
BrainGhost <juniorbalamage@gmail.com>
2021-08-19 14:39:08 +0300

    Fourth saved(Delete option and animation, preloader, scrool inside right side div, add function but getting data from the input)

--- app.js
+++ app.js
@@ -35,3 +35,72 @@ TabNavigation.forEach((link) => {
     event.target.classList.add("active");
   });
 });
+//Can use the Enter 13 === Enter Button to save data
+document.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    addSearch();
+  }
+});
+
+//function to search and add data
+const cardHolder = document.querySelector(".card-container");
+const inputSearch = document.querySelector(".input-search");
+const btnSearch = document.querySelector(".btn-search");
+
+btnSearch.addEventListener("click", addSearch);
+
+function addSearch() {
+  if (inputSearch.value === 0) {
+    alert("message?: Please insert correct value");
+  } else {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("addEffect");
+    const cardName = document.createElement("div");
+    cardName.classList.add("name");
+    const cardFavIcon = document.createElement("button");
+    cardFavIcon.classList.add("trash-icon");
+    cardFavIcon.innerHTML = '<i class="ti-trash"></i>';
+
+    const cardTitle = document.createElement("h3");
+    cardTitle.innerHTML = inputSearch.value;
+
+    cardName.appendChild(cardFavIcon);
+    cardName.appendChild(cardTitle);
+    card.appendChild(cardName);
+
+    const cardDetails = document.createElement("div");
+    cardDetails.classList.add("details");
+    card.appendChild(cardDetails);
+
+    cardHolder.appendChild(card);
+    inputSearch.value = "";
+  }
+}
+
+//function to remove data
+cardHolder.addEventListener("click", deleteCheck);
+
+function deleteCheck(e) {
+  const item = e.target;
+  if (item.className === "trash-icon") {
+    console.log("delete");
+    const toRemove = item.parentElement.parentElement;
+    toRemove.classList.add("deleteEffect");
+    toRemove.addEventListener("transtioned", () => {
+      toRemove.remove();
+    });
+    console.log(toRemove);
+  }
+}
+//Fetch API
+
+//Store to local Storage
+
+//On load
+const loading = document.querySelector(".loading");
+window.addEventListener("load", () => {
+  loading.className += " hidden"; //the space is necessary unless it wont work
+  // loading.classList.add("hidden");
+  // loading.style.display = "none";
+});
