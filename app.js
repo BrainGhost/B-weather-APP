0a7196cecd386e56af70c56430a126ba8d0c8fa0
BrainGhost <juniorbalamage@gmail.com>
2021-08-25 20:27:19 +0300

    Sixth commits saved (Fetch data from the Api and display the current city location and time, bg-color saved to the local storage)

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener("load", () => {
   loading.className += " hidden"; //the space is necessary unless it wont work
   // loading.classList.add("hidden");
   // loading.style.display = "none";
+  getUserLocation();
+  getUserTime();
 });
 
 // Declaration of variable
@@ -43,24 +45,68 @@ TabNavigation.forEach((link) => {
     event.target.classList.add("active");
   });
 });
-//Can use the Enter 13 === Enter Button to save data
-document.addEventListener("keyup", (event) => {
+//function to change bg-colors===========================================>
+const box_time = document.querySelector(".box-time");
+const bgColor = document.querySelectorAll(".bg-color span");
+const link_change = document.querySelector("#bg-link-change");
+const btnMode = document.querySelectorAll(".circle");
+//fetch from local storage
+let theme = localStorage.getItem("theme");
+if (theme === null) {
+  setMode("blue");
+} else {
+  setMode(theme);
+}
+
+btnMode.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const mode = btn.dataset.mode;
+    const activeBtn = document.querySelectorAll(".active");
+    activeBtn.forEach((e) => {
+      e.className = e.className.replace("active", "");
+    });
+    btn.classList.add("active");
+
+    setMode(mode);
+  });
+});
+function setMode(mode) {
+  if (mode === "blue") {
+    link_change.href = "./styles.css";
+  }
+  if (mode === "red") {
+    link_change.href = "./themes/red.css";
+  }
+  if (mode === "yellow") {
+    link_change.href = "./themes/yellow.css";
+  }
+  if (mode === "green") {
+    link_change.href = "./themes/green.css";
+  }
+  localStorage.setItem("theme", mode);
+}
+
+//Can use the Enter 13 === Enter Button to save data=================================>
+document.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
+    event.preventDefault;
     addSearch();
   }
 });
 
 //function to search and add data
 const cardHolder = document.querySelector(".card-container");
-const inputSearch = document.querySelector(".input-search");
+let inputSearch = document.querySelector(".input-search");
 const btnSearch = document.querySelector(".btn-search");
-
 btnSearch.addEventListener("click", addSearch);
 
 function addSearch() {
   if (inputSearch.value === 0) {
     alert("message?: Please insert correct value");
   } else {
+    let searchLocation = inputSearch.value;
+    fetchResult(searchLocation);
+    cardAdd();
     const card = document.createElement("div");
     card.classList.add("card");
     card.classList.add("addEffect");
@@ -101,56 +147,91 @@ function deleteCheck(e) {
     console.log(toRemove);
   }
 }
-//Fetch API
+//Fetch API======================================================================>
 
 //Store to local Storage
+
+//Varible of items to filled from the API data
+const cityTemp = document.querySelector(".temperature");
+const cityName = document.querySelector(".city-name");
+const cityIcon = document.querySelector("#city-icon");
+const cityHumidity = document.querySelector("#humidity");
+const cityDescription = document.querySelector("#description");
+
+//variables for the API
+const apiKey = "62366ce36a3b0ee777207eb6626edd19";
+
 //get request
-// fetch("")
-//   //handle success
-//   .then((response) => response.json())
-//   //convert to json
-//   .then((json) => {
-//     console.log(json);
-//   }) //print data to console
-//   .catch((err) => {
-//     alert.log("Could not fetch the API");
-//   }); //catch errors
+function fetchResult(location) {
+  fetch(
+    // "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=metric&exclude=daily&appid=62366ce36a3b0ee777207eb6626edd19"
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`
+  )
+    //handle success
+    .then((response) => response.json()) //convert to json
+    .then((data) => {
+      let temperature, humidity, description, name;
+      temperature = data.main.temp;
+      let icon =
+        "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+      name = data.name;
 
-//function to change bg-colors
-const box_time = document.querySelector(".box-time");
+      humidity = data.main.humidity;
+      description = data.weather[0].description;
+      //Fill data gotten from the API to the Interface
+      cityTemp.innerText = Math.floor(temperature);
+      cityName.innerText = name;
+      cityIcon.src = icon;
+      cityHumidity.innerText = humidity;
+      cityDescription.innerText = description;
+    }) //print data to console
+    .catch((err) => alert("Could not fetch data for that city")); //catch errors
 
-const bgColor = document.querySelectorAll(".bg-color span");
-bgColor.forEach((span) => {
-  span.addEventListener("click", (event) => {
-    const active_btn = document.querySelectorAll(".active");
-    active_btn.forEach((e) => {
-      e.className = e.className.replace("active", "");
-    });
-    event.target.classList.add("active");
-    const link_change = document.querySelector("#bg-link-change");
+  cardAdd(cityName, cityTemp);
+}
+// Get the current user location
+let latitude, longitude;
+const currentCityName = document.querySelector("h1#name");
+const currentCityTime = document.querySelector("span#time");
+function getUserLocation() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
 
-    const link = event.target.classList;
-    //Store colors for the background
-    localStorage.setItem("themes", link[1]);
-
-    if (link.contains("active")) {
-      if (link[1] === "blue-color") {
-        link_change.href = "./styles.css";
-      }
-      if (link[1] === "red-color") {
-        link_change.href = "./themes/red.css";
-      }
-      if (link[1] === "yellow-color") {
-        link_change.href = "./themes/yellow.css";
-      }
-      if (link[1] === "green-color") {
-        link_change.href = "./themes/green.css";
-      }
-    } else {
-      alert("Message: Are sure your code is correct??LOL");
-    }
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
+    )
+      .then((reponse) => reponse.json())
+      .then((data) => {
+        let icon =
+          "http://openweathermap.org/img/wn/" +
+          data.weather[0].icon +
+          "@2x.png";
+        cityTemp.innerText = Math.floor(data.main.temp);
+        cityName.innerText = data.name;
+        cityIcon.src = icon;
+        cityHumidity.innerText = data.main.humidity;
+        cityDescription.innerText = data.weather[0].description;
+        currentCityName.innerText = data.name;
+      });
   });
-});
-//theme on window refresh
-const colorFromLocalStorage = localStorage.getItem("themes");
-console.log(colorFromLocalStorage);
+}
+//get user time
+let date, dayName, time, day;
+function getUserTime() {
+  date = new Date();
+  time = `${date.getHours()}:${date.getMinutes()}`;
+  currentCityTime.innerHTML = time;
+}
+// Check if the user is connected to Internet if not display a page
+
+//Use the other API to fetch data for 10 days, hourly etc
+
+// get the Item search to display in the cards
+function cardAdd(cityName, cityTemp) {
+  const cardName = (document.querySelectorAll("h3#card-name").innerText =
+    cityName);
+  const cardTemp = (document.querySelectorAll("div#card-temp").innerText =
+    cityTemp);
+  console.log(cardName, cardTemp);
+}
